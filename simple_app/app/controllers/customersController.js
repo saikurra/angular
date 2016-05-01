b782eb3c8464a7cf6ff4cba1c7e12f97856a313a
srinadh.a840@gmail.com <srinadh.a840@gmail.com>
2016-05-01 02:31:04 -0500

    Writing factory and using in controller

--- simple_app/app/controllers/customersController.js
+++ simple_app/app/controllers/customersController.js
@@ -1,45 +1,17 @@
 //Option 3
 //wrapper function will make names not in global scope, good in long run working on big applications
 (function () {
-    var CustomersController = function ($scope) {
+    var CustomersController = function ($scope, customersFactory) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
-        $scope.customers = [
-            {
-                id: 1,
-                joined: '2000-12-02',
-                name: 'John',
-                city: 'Chandler',
-                orderTotal: 9.9956
-        },
-            {
-                id: 2,
-                joined: '1965-01-25',
-                name: 'Zed',
-                city: 'Las Vegas',
-                orderTotal: 19.99
-        },
-            {
-                id: 3,
-                joined: '1944-06-15',
-                name: 'Tina',
-                city: 'New York',
-                orderTotal: 44.99
-        }, {
-                id: 4,
-                joined: '1995-03-28',
-                name: 'Dave',
-                city: 'Seattle',
-                orderTotal: 101.50
-            }
-            ];
+        $scope.customers = customersFactory.getCustomers();
         $scope.doSort = function (propName) {
             $scope.sortBy = propName;
             $scope.reverse = !$scope.reverse;
         };
     };
 
-    CustomersController.$inject = ['$scope'];
+    CustomersController.$inject = ['$scope', 'customersFactory'];
     angular.module('customersApp').controller('CustomersController', CustomersController);
 }());
 
