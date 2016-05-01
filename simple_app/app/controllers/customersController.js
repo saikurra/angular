3cd3faccc767ef152472ff61c1ca162a42271bc5
srinadh.a840@gmail.com <srinadh.a840@gmail.com>
2016-05-01 00:48:15 -0500

    Parameter Injection added

--- simple_app/app/controllers/customersController.js
+++ simple_app/app/controllers/customersController.js
@@ -1,3 +1,45 @@
+//Option 3
+//wrapper function will make names not in global scope, good in long run working on big applications
+(function () {
+    var CustomersController = function ($scope) {
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
+        $scope.customers = [
+            {
+                joined: '2000-12-02',
+                name: 'John',
+                city: 'Chandler',
+                orderTotal: 9.9956
+        },
+            {
+                joined: '1965-01-25',
+                name: 'Zed',
+                city: 'Las Vegas',
+                orderTotal: 19.99
+        },
+            {
+                joined: '1944-06-15',
+                name: 'Tina',
+                city: 'New York',
+                orderTotal: 44.99
+        }, {
+
+                joined: '1995-03-28',
+                name: 'Dave',
+                city: 'Seattle',
+                orderTotal: 101.50
+            }
+            ];
+        $scope.doSort = function (propName) {
+            $scope.sortBy = propName;
+            $scope.reverse = !$scope.reverse;
+        };
+    };
+
+    CustomersController.$inject = ['$scope'];
+    angular.module('customersApp').controller('CustomersController', CustomersController);
+}());
+
 //Option 1
 /*app.controller('CustomersController', function ($scope) {
     $scope.sortBy = 'name';
@@ -73,44 +115,3 @@
     };
 }());
 */
-
-//Option 3
-
-(function () {
-    var CustomersController = function ($scope) {
-        $scope.sortBy = 'name';
-        $scope.reverse = false;
-        $scope.customers = [
-            {
-                joined: '2000-12-02',
-                name: 'John',
-                city: 'Chandler',
-                orderTotal: 9.9956
-        },
-            {
-                joined: '1965-01-25',
-                name: 'Zed',
-                city: 'Las Vegas',
-                orderTotal: 19.99
-        },
-            {
-                joined: '1944-06-15',
-                name: 'Tina',
-                city: 'New York',
-                orderTotal: 44.99
-        },
-            {
-                joined: '1995-03-28',
-                name: 'Dave',
-                city: 'Seattle',
-                orderTotal: 101.50
-        }
-    ];
-        $scope.doSort = function (propName) {
-            $scope.sortBy = propName;
-            $scope.reverse = !$scope.reverse;
-        };
-    };
-    
-    angular.module('customersApp').controller('CustomersController', CustomersController);
-}());
